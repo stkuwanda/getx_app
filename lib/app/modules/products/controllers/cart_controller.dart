c9a886ffaeca1e0f137fbe0f80477875d8cf384f
Simbarashe Tanaka Kuwanda <stkuwanda@gmail.com>
2026-02-07 20:11:03 +0200

    create cart controller

---
+++ lib/app/modules/products/controllers/cart_controller.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+import 'package:getx_app/app/modules/products/models/product_model.dart';
+
+class CartController extends GetxController {
+  var cartItems = <ProductModel>[].obs;
+  var totalAmount = 0.0.obs;
+
+  void addToCart(ProductModel product) {
+    cartItems.add(product);
+    calculateTotal();
+  }
+
+  void removeFromCart(ProductModel product) {
+    cartItems.remove(product);
+  }
+
+  void calculateTotal() {
+    totalAmount.value = cartItems.fold(0.0, (sum, item) => sum + item.price);
+    calculateTotal();
+  }
+}
