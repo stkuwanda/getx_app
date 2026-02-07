60416e12f355f96821aa34deb55c3c4432ac40b9
Simbarashe Tanaka Kuwanda <stkuwanda@gmail.com>
2026-02-07 07:13:56 +0200

    create and configure GetX routes

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,28 @@
+// This file defines the routes used in the application. Each route is associated with a 
+// specific view or page. 'part 'app_routes.dart'; means that this file is a part of the 
+// 'app_pages.dart' file, which likely contains the overall routing configuration for the 
+// application. The Routes class defines static constants for each route, making it 
+// easier to manage and reference routes throughout the app. For example, when navigating 
+// to the product details page, you can use Routes.PRODUCT_DETAILS instead of hardcoding 
+// the string '/product-details'.
+
+import 'package:get/get.dart';
+import 'package:getx_app/app/modules/products/views/product_details_view.dart';
+import 'package:getx_app/app/modules/products/views/product_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  // static const INITIAL = Routes.PRODUCTS; // Define the initial route of the application
+
+  static final routes = [
+    GetPage(
+      name: Routes.PRODUCT,
+      page: () => ProductView(), // Associate the PRODUCTS route with the ProductView
+    ),
+    GetPage(
+      name: Routes.PRODUCT_DETAILS,
+      page: () => ProductDetailsView(), // Associate the PRODUCT_DETAILS route with the ProductDetailsView
+    ),
+  ];
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,13 @@
+// This file defines the routes used in the application. Each route is associated with a 
+// specific view or page.'part of 'app_pages.dart'; means that this file is a part of the 
+// 'app_pages.dart' file, which likely contains the overall routing configuration for the 
+// application. The Routes class defines static constants for each route, making it easier 
+// to manage and reference routes throughout the app. For example, when navigating to the 
+// product details page, you can use Routes.PRODUCT_DETAILS instead of hardcoding the 
+// string '/product-details'.
+part of 'app_pages.dart'; 
+
+abstract class Routes {
+  static const PRODUCT = '/product';
+  static const PRODUCT_DETAILS = '/product-details';
+}
