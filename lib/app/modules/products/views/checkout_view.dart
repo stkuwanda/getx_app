dd42e46e3e044d86a2bf10a6ff3d9a94464d11dd
Simbarashe Tanaka Kuwanda <stkuwanda@gmail.com>
2026-02-08 12:57:47 +0200

    create checkout view

---
+++ lib/app/modules/products/views/checkout_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CheckoutView extends StatelessWidget {
+  const CheckoutView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Checkout')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Thank you for your purchase.'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Get.offAllNamed('/product'); // Navigate back to the product list and clear the navigation stack
+              },
+              child: const Text('Go to Home'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
