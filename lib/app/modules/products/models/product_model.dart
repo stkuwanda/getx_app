9f090e631d55000bbc54a0fd40bf07d435cc2a8a
Simbarashe Tanaka Kuwanda <stkuwanda@gmail.com>
2026-02-08 22:41:03 +0200

    update ProductModel with serializer and deserializer

--- lib/app/modules/products/models/product_model.dart
+++ lib/app/modules/products/models/product_model.dart
@@ -1,6 +1,13 @@
 class ProductModel {
   const ProductModel({required this.name, required this.price});
 
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+    name: json['name'],
+    price: (json['price'] as num).toDouble(),
+  );
+
   final String name;
   final double price;
+
+  Map<String, dynamic> toJson() => {'name': name, 'price': price};
 }
