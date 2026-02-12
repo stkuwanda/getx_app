6a5334b88e7f687169be758938c1733ec39f9ab1
Simbarashe Tanaka Kuwanda <stkuwanda@gmail.com>
2026-02-12 09:36:20 +0200

    cretate theme service, configure theme and theme mode

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_app/app/routes/app_pages.dart';
+import 'package:getx_app/app/services/theme_service.dart';
 import 'package:getx_app/app/services/translations.dart';
 
 class GetXApp extends StatelessWidget {
@@ -13,9 +14,9 @@ class GetXApp extends StatelessWidget {
     translations: AppTranslations(),
     locale: Locale('en', 'US'),
     fallbackLocale: Locale('en', 'US'),
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
+    theme: ThemeData.light(), // Define the light theme for the application
+    darkTheme: ThemeData.dark(), // Define the dark theme for the application
+    themeMode: ThemeService().theme, // Set the theme mode based on the user's preference
     initialRoute: AppPages.initial, // Set the initial route of the application
     getPages: AppPages.routes, // Define the routes for the application
   );
---
+++ lib/app/services/theme_service.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ThemeService {
+  // final _key = 'isDarkMode';
+
+  bool _loadThemeFromStorage() => Get.isDarkMode;
+
+  void _saveThemeToStorage(bool isDarkMode) =>
+      Get.changeThemeMode(isDarkMode ? ThemeMode.dark : ThemeMode.light);
+
+  ThemeMode get theme =>
+      _loadThemeFromStorage() ? ThemeMode.dark : ThemeMode.light;
+
+  void switchTheme() {
+    _saveThemeToStorage(!_loadThemeFromStorage());
+  }
+}
